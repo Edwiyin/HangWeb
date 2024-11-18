8b63d4462f7b173f69c09c7ded5a09a0f020d24a
EdwinWehbe07 <edwin.wehbe@ynov.com>
2024-11-18 15:46:41 +0100

    Add uused letter,fix double letter problem

--- main.go
+++ main.go
@@ -77,18 +77,53 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		hiddenWord := strings.Repeat("_ ", len(word)-1)
-		for i := 0; i < revealedLetters; i++ {
-			randIndex := -1
-			oldIndex := -1
-			if randIndex != -1 {
-				oldIndex = randIndex
-			}
-			randIndex = rand.Intn(len(word))
+		usedLetters := ""
 
-			for randIndex == oldIndex {
-				randIndex = rand.Intn(len(word))
+		letterPositions := make(map[rune][]int)
+		for i, letter := range word {
+			letterPositions[letter] = append(letterPositions[letter], i)
+		}
+		revealedPositions := make(map[int]bool)
+
+		for i := 0; i < revealedLetters && i < len(word); i++ {
+			var candidateLetters []rune
+			for letter, positions := range letterPositions {
+				if len(positions) > 1 && !revealedPositions[positions[0]] && !revealedPositions[positions[1]] {
+					candidateLetters = append(candidateLetters, letter)
+				}
 			}
-			hiddenWord = hiddenWord[:randIndex*2] + string(word[randIndex]) + hiddenWord[randIndex*2+1:]
+
+			var randIndex int
+			var revealedLetter rune
+
+			if len(candidateLetters) > 0 {
+
+				revealedLetter = candidateLetters[rand.Intn(len(candidateLetters))]
+				positions := letterPositions[revealedLetter]
+
+				for _, pos := range positions {
+					if !revealedPositions[pos] {
+						randIndex = pos
+						break
+					}
+				}
+			} else {
+
+				for {
+					randIndex = rand.Intn(len(word))
+					if !revealedPositions[randIndex] {
+						revealedLetter = rune(word[randIndex])
+						break
+					}
+				}
+			}
+
+			revealedPositions[randIndex] = true
+			hiddenWord = hiddenWord[:randIndex*2] + string(revealedLetter) + hiddenWord[randIndex*2+1:]
+
+			if !strings.Contains(usedLetters, string(revealedLetter)) {
+				usedLetters += string(revealedLetter)
+			}
 		}
 
 		setCookie(w, "username", username)
@@ -96,7 +131,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		setCookie(w, "word", word)
 		setCookie(w, "hiddenWord", hiddenWord)
 		setCookie(w, "attempts", attempts)
-		setCookie(w, "usedLetters", "")
+		setCookie(w, "usedLetters", usedLetters)
 
 		http.Redirect(w, r, "/game", http.StatusSeeOther)
 		return
@@ -131,7 +166,6 @@ func gameHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func submitHandler(w http.ResponseWriter, r *http.Request) {
-
 	username, _ := getCookie(r, "username")
 	word, _ := getCookie(r, "word")
 	hiddenWord, _ := getCookie(r, "hiddenWord")
@@ -143,36 +177,68 @@ func submitHandler(w http.ResponseWriter, r *http.Request) {
 	guess := r.FormValue("guess")
 	message := ""
 	messageType := ""
+
+	letterCount := strings.Count(word, guess)
+
+	revealedCount := strings.Count(hiddenWord, guess)
+
 	if strings.Contains(usedLetters, guess) {
-		message = "La lettre '" + guess + "' a déjà été utilisée!"
-		messageType = "warning"
-		image = strconv.Itoa(10 - attempts)
-	} else {
-		usedLetters += guess
-		setCookie(w, "usedLetters", usedLetters)
-		if len(guess) > 1 {
-			if guess == word {
+
+		if letterCount > revealedCount {
+
+			message = "Correct!"
+			messageType = "success"
+			image = strconv.Itoa(10 - attempts)
+
+			for i, letter := range word {
+				if string(letter) == guess &&
+					hiddenWord[i*2:i*2+1] == "_" {
+					hiddenWord = hiddenWord[:i*2] + guess + hiddenWord[i*2+1:]
+				}
+			}
+
+			if strings.Replace(hiddenWord, " ", "", -1) == word {
 				messageType = "success"
-				hiddenWord = word
-				attempts = 0
-				image = "won"
 				message = "Congratulations, you won!"
-				word, _ := getCookie(r, "word")
-				username, _ := getCookie(r, "username")
-				attemptsStr, _ := getCookie(r, "attempts")
-				difficulty, _ := getCookie(r, "difficulty")
 				score_board_entry := username + "," + word + "," + attemptsStr + "," + difficulty
 				f, err := os.OpenFile("./scoreboard.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 				check(err)
 				f.WriteString(score_board_entry + "\n")
 				attempts = 0
+			}
+		} else {
+			if revealedCount > 1 {
+				message = "Toutes les occurrences de cette lettre ont été révélées !"
+				messageType = "warning"
+				image = strconv.Itoa(10 - attempts)
+			} else {
+				message = "Vous avez déjà utilisé cette lettre !"
+				messageType = "warning"
+				image = strconv.Itoa(10 - attempts)
+			}
+		}
+
+	} else {
+		usedLetters += guess
+		setCookie(w, "usedLetters", usedLetters)
+
+		if len(guess) > 1 {
+			if guess == word {
+				messageType = "success"
+				hiddenWord = word
+				image = "won"
+				attempts = 0
+				message = "Congratulations, you won!"
+				score_board_entry := username + "," + word + "," + attemptsStr + "," + difficulty
+				f, err := os.OpenFile("./scoreboard.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+				check(err)
+				f.WriteString(score_board_entry + "\n")
 			} else {
 				attempts -= 2
 				message = "Incorrect!"
 				messageType = "error"
 				image = strconv.Itoa(10 - attempts)
 			}
-
 		} else {
 			if strings.Contains(word, guess) {
 				message = "Correct!"
@@ -186,10 +252,7 @@ func submitHandler(w http.ResponseWriter, r *http.Request) {
 				if strings.Replace(hiddenWord, " ", "", -1) == word {
 					messageType = "success"
 					message = "Congratulations, you won!"
-					word, _ := getCookie(r, "word")
-					username, _ := getCookie(r, "username")
-					attemptsStr, _ := getCookie(r, "attempts")
-					difficulty, _ := getCookie(r, "difficulty")
+					image = "won"
 					score_board_entry := username + "," + word + "," + attemptsStr + "," + difficulty
 					f, err := os.OpenFile("./scoreboard.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 					check(err)
